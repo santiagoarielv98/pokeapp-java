58986ba684085edddd13a86fda6f9673b9bb41c9
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-16 13:46:10 -0300

    cambios

---
+++ src/main/java/com/svillanueva/pokeapp/pokeapi/client/PokeApiClient.java
@@ -0,0 +1,43 @@
+package com.svillanueva.pokeapp.pokeapi.client;
+
+import com.google.gson.Gson;
+import com.svillanueva.pokeapp.pokeapi.dto.NamedAPIResourceList;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+
+@ApplicationScoped
+public class PokeApiClient {
+  private Client client;
+  private WebTarget webTarget;
+  private Gson gson;
+
+  @PostConstruct
+  public void init() {
+    client = ClientBuilder.newClient();
+    webTarget = client.target("https://pokeapi.co/api/v2");
+    gson = new Gson();
+  }
+
+  @PreDestroy
+  public void destroy() {
+    client.close();
+  }
+
+  public <T> T get(String path, Class<T> responseType) {
+    return gson.fromJson(webTarget
+        .path(path)
+        .request(MediaType.APPLICATION_JSON)
+        .get(String.class),
+        responseType);
+  }
+
+  NamedAPIResourceList listPokemon() {
+    return get("/pokemon", NamedAPIResourceList.class);
+  }
+}
---
+++ src/main/java/com/svillanueva/pokeapp/pokeapi/dto/NamedAPIResource.java
@@ -0,0 +1,14 @@
+package com.svillanueva.pokeapp.pokeapi.dto;
+
+public class NamedAPIResource {
+  private String name;
+  private String url;
+
+  public String getName() {
+    return name;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+}
